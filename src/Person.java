389ca8bc8d453d666f4284a9317acb1f81d4ba77
ivanpopovic25 <ivanpopovic25@gmail.com>
2024-10-21 11:24:04 +0200

    first commit

---
+++ src/Person.java
@@ -0,0 +1,22 @@
+public class Person {
+
+    protected String ime;
+    protected String prezime;
+    protected String email;
+    protected Adresa adresa;
+    
+    public Person(String ime, String prezime, String email, Adresa adresa) {
+        this.ime = ime;
+        this.prezime = prezime;
+        this.email = email;
+        this.adresa = adresa;
+    }
+   
+    
+    }
+
+
+    
+
+    
+}
---
+++ src/Student.java
@@ -0,0 +1,16 @@
+public class Student extends Person {
+
+    String brIndexa;
+
+    public Student(String ime, String prezime, String email, Adresa adresa, String brIndexa) {
+        super(ime, prezime, email, adresa);
+        this.brIndexa = brIndexa;
+        //TODO Auto-generated constructor stub
+    }
+
+    
+
+   
+    }
+    
+}
